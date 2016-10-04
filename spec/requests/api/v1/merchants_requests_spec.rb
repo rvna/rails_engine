96c383a73dc7c2623c32f1f219e193fbe76d33bf
Ryan Workman <rdavid1099@gmail.com>
2016-10-03 18:16:23 -0600

    Adds merchant show to return JSON of specific merchant, raises exception if ID does not exist

--- spec/requests/api/v1/merchants_requests_spec.rb
+++ spec/requests/api/v1/merchants_requests_spec.rb
@@ -8,6 +8,27 @@ describe 'merchants endpoints functioning' do
 
     expect(response.status).to eq(200)
     expect(merchants.count).to eq(3)
-    expect(merchants[0]["id"]).to eq(Merchant.first.id)
+    expect(merchants[0]['id']).to eq(Merchant.first.id)
+  end
+
+  it 'returns JSON for specific merchant' do
+    expected = create(:merchant, name: 'Dude')
+    get "/api/v1/merchants/#{expected.id}"
+    actual = JSON.parse(response.body)
+
+    expect(response.status).to eq(200)
+    expect(actual['id']).to eq(expected.id)
+    expect(actual['name']).to eq('Dude')
+  end
+
+  it 'raises exception if merchant id is not found' do
+    expected = create(:merchant, name: 'Dude')
+    get "/api/v1/merchants/0"
+    actual = JSON.parse(response.body)
+
+    expect(response.status).to eq(404)
+    expect(actual['id']).to eq(nil)
+    expect(actual['name']).to eq(nil)
+    expect(actual['error']).to eq('not-found')
   end
 end
