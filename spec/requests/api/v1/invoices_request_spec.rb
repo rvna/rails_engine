7ffc4e119098d637ca4c4ed431e74d729490ba91
Brendan Dillon <br.lyle.dillon@gmail.com>
2016-10-04 16:30:41 -0600

    Create serializers for find and index on invoices

--- spec/requests/api/v1/invoices_request_spec.rb
+++ spec/requests/api/v1/invoices_request_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe 'Invoices API' do
   it 'finds an invoice by id' do
     invoice = create(:invoice, status: 'shipped')
 
-    get "/api/v1/invoices/find?id=#{invoice.id}"
+    get "/api/v1/invoices/find.json?id=#{invoice.id}"
     output = JSON.parse(response.body)
 
     expect(response.status).to eq(200)
@@ -34,7 +34,7 @@ RSpec.describe 'Invoices API' do
   it 'finds an invoice by status' do
     create(:invoice, id: 2, status: 'shipped')
 
-    get '/api/v1/invoices/find?status=shipped'
+    get '/api/v1/invoices/find.json?status=shipped'
     output = JSON.parse(response.body)
 
     expect(response.status).to eq(200)
@@ -44,7 +44,7 @@ RSpec.describe 'Invoices API' do
   it 'finds an invoice by the time it was created' do
     create(:invoice, status: 'shipped', created_at: '1999-01-01')
     
-    get '/api/v1/invoices/find?created_at=1999-01-01'
+    get '/api/v1/invoices/find.json?created_at=1999-01-01'
     output = JSON.parse(response.body)
 
     expect(response.status).to eq(200)
@@ -54,7 +54,7 @@ RSpec.describe 'Invoices API' do
   it 'finds an invoice by the time it was updated' do
     create(:invoice, status: 'shipped', updated_at: '1999-01-01')
     
-    get '/api/v1/invoices/find?updated_at=1999-01-01'
+    get '/api/v1/invoices/find.json?updated_at=1999-01-01'
     output = JSON.parse(response.body)
 
     expect(response.status).to eq(200)
@@ -64,7 +64,7 @@ RSpec.describe 'Invoices API' do
   it 'finds multiple invoices by status' do
     create_list(:invoice, 2, status: 'shipped')
 
-    get '/api/v1/invoices/find_all?status=shipped'
+    get '/api/v1/invoices/find_all.json?status=shipped'
     output = JSON.parse(response.body)
 
     expect(response.status).to eq(200)
@@ -74,7 +74,7 @@ RSpec.describe 'Invoices API' do
   it 'finds a random invoice' do
     create(:invoice, status: 'shipped')
 
-    get '/api/v1/invoices/random'
+    get '/api/v1/invoices/random.json'
     output = JSON.parse(response.body)
 
     expect(response.status).to eq(200)
