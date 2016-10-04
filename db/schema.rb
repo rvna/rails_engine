df66f29b57399a88d81c28922dcbbf9b7414a602
Ryan Workman <rdavid1099@gmail.com>
2016-10-04 08:54:57 -0600

    Fix foreign key for transactions linking to invoices

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161004134604) do
+ActiveRecord::Schema.define(version: 20161004144536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,14 +54,16 @@ ActiveRecord::Schema.define(version: 20161004134604) do
   end
 
   create_table "transactions", force: :cascade do |t|
-    t.integer  "invoice_id"
     t.string   "credit_card_number"
     t.integer  "credit_card_expiration_date"
     t.string   "result"
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+    t.integer  "invoice_id"
+    t.index ["invoice_id"], name: "index_transactions_on_invoice_id", using: :btree
   end
 
   add_foreign_key "invoice_items", "invoices"
   add_foreign_key "invoice_items", "items"
+  add_foreign_key "transactions", "invoices"
 end
