264a1a794d7342dcca84a52340b273c3b7545576
Brendan Dillon <br.lyle.dillon@gmail.com>
2016-10-03 19:06:20 -0600

    Make invoice find able to search by created at and updated at

--- spec/requests/api/v1/invoices_request_spec.rb
+++ spec/requests/api/v1/invoices_request_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Invoices API' do
   it 'returns a list of invoices' do
     create_list(:invoice, 3)
+
     get '/api/v1/invoices'
     output = JSON.parse(response.body)
 
@@ -12,6 +13,7 @@ RSpec.describe 'Invoices API' do
   
   it 'returns an individual invoice' do
     invoice = create(:invoice, status: 'shipped')
+
     get "/api/v1/invoices/#{invoice.id}"
     output = JSON.parse(response.body)
 
@@ -21,6 +23,7 @@ RSpec.describe 'Invoices API' do
 
   it 'finds an invoice by id' do
     invoice = create(:invoice, status: 'shipped')
+
     get "/api/v1/invoices/find?id=#{invoice.id}"
     output = JSON.parse(response.body)
 
@@ -30,11 +33,32 @@ RSpec.describe 'Invoices API' do
 
   it 'finds an invoice by status' do
     create(:invoice, id: 2, status: 'shipped')
+
     get '/api/v1/invoices/find?status=shipped'
     output = JSON.parse(response.body)
 
     expect(response.status).to eq(200)
     expect(output["id"]).to eq(2)
+  end
+
+  it 'finds an invoice by the time it was created' do
+    create(:invoice, status: 'shipped', created_at: '1999-01-01')
+    
+    get '/api/v1/invoices/find?created_at=1999-01-01'
+    output = JSON.parse(response.body)
+
+    expect(response.status).to eq(200)
+    expect(output["status"]).to eq('shipped')
+  end
+
+  it 'finds an invoice by the time it was updated' do
+    create(:invoice, status: 'shipped', updated_at: '1999-01-01')
+    
+    get '/api/v1/invoices/find?updated_at=1999-01-01'
+    output = JSON.parse(response.body)
+
+    expect(response.status).to eq(200)
+    expect(output["status"]).to eq('shipped')
   end
     
 end
