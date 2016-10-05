fed5ffafe202f8457e21e9ff1b62c0b91b89f4e8
Ryan Workman <rdavid1099@gmail.com>
2016-10-05 13:29:50 -0600

    Fix minor rounding issue when searching for items by price

--- app/controllers/api/v1/items/finder_controller.rb
+++ app/controllers/api/v1/items/finder_controller.rb
@@ -11,7 +11,7 @@ class Api::V1::Items::FinderController < ApplicationController
 
   def item_params
     if params.keys.include?('unit_price')
-      {unit_price: (params['unit_price'].to_f * 100)}
+      {unit_price: (params['unit_price'].to_f * 100).round}
     else
       params.permit(:id, :name, :description, :created_at, :updated_at, :merchant_id)
     end
