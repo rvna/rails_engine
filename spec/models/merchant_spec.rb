11a4929afed114f451c8c9610f01c95cf5cd23a8
Brendan Dillon <br.lyle.dillon@gmail.com>
2016-10-06 15:31:50 -0600

    Add model test for total revenue by date

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -66,4 +66,27 @@ RSpec.describe Merchant, type: :model do
     expect(result.length).to eq(2)
   end
 
+  it 'finds the total revenue across all merchants by date' do
+    merchant1 = create(:merchant, name: 'Merch1')
+    invoice1 = create(:invoice, merchant_id: merchant1.id, created_at: '2012-03-16 11:56:05')
+    create(:invoice_item, invoice_id: invoice1.id, unit_price: 200, quantity: 2)
+    create(:transaction, invoice_id: invoice1.id, result: 'success')
+    merchant2 = create(:merchant, name: 'Merch2')
+    invoice2 = create(:invoice, merchant_id: merchant2.id, created_at: '2012-03-16 11:56:05')
+    create(:invoice_item, invoice_id: invoice2.id, unit_price: 500)
+    create(:transaction, invoice_id: invoice2.id, result: 'success')
+    merchant3 = create(:merchant, name: 'Merch3')
+    invoice3 = create(:invoice, merchant_id: merchant3.id, created_at: '2012-03-16 11:56:05')
+    create(:invoice_item, invoice_id: invoice3.id, unit_price: 500)
+    create(:transaction, invoice_id: invoice3.id, result: 'failed')
+    merchant4 = create(:merchant, name: 'Merch4')
+    invoice4 = create(:invoice, merchant_id: merchant4.id, created_at: '2012-04-16 11:55:05')
+    create(:invoice_item, invoice_id: invoice4.id, unit_price: 100)
+    create(:transaction, invoice_id: invoice4.id, result: 'success')
+
+    result = Merchant.total_revenue_by_day('2012-03-16 11:56:05')
+
+    expect(result).to eq('9.00')
+  end
+
 end
