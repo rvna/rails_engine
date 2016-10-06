19bde6f775ed48227a1675de8b8b1d0de726963c
Brendan Dillon <br.lyle.dillon@gmail.com>
2016-10-06 15:29:07 -0600

    Add model test for top merchants by revenue

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -41,4 +41,29 @@ RSpec.describe Merchant, type: :model do
     expect(result.length).to eq(2)
   end
     
+  it 'returns the top x merchants ranked by total revenue' do
+    merchant1 = create(:merchant, name: 'Merch1')
+    invoice1 = create(:invoice, merchant_id: merchant1.id)
+    create(:invoice_item, invoice_id: invoice1.id, unit_price: 200)
+    create(:transaction, invoice_id: invoice1.id, result: 'success')
+    merchant2 = create(:merchant, name: 'Merch2')
+    invoice2 = create(:invoice, merchant_id: merchant2.id)
+    create(:invoice_item, invoice_id: invoice2.id, unit_price: 500)
+    create(:transaction, invoice_id: invoice2.id, result: 'success')
+    merchant3 = create(:merchant, name: 'Merch3')
+    invoice3 = create(:invoice, merchant_id: merchant3.id)
+    create(:invoice_item, invoice_id: invoice3.id, unit_price: 500)
+    create(:transaction, invoice_id: invoice3.id, result: 'failed')
+    merchant4 = create(:merchant, name: 'Merch4')
+    invoice4 = create(:invoice, merchant_id: merchant4.id)
+    create(:invoice_item, invoice_id: invoice4.id, unit_price: 100)
+    create(:transaction, invoice_id: invoice4.id, result: 'success')
+
+    result = Merchant.most_revenue(2)
+
+    expect(result[0]['name']).to eq('Merch2')
+    expect(result[1]['name']).to eq('Merch1')
+    expect(result.length).to eq(2)
+  end
+
 end
