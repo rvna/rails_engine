e78ae4e3ecc280da6b64184a1cf6ebc7b0118516
Brendan Dillon <br.lyle.dillon@gmail.com>
2016-10-06 15:19:43 -0600

    Add model test for total revenue for single merchant

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
+  it 'returns a given merchants total revenue' do
+    merchant = create(:merchant)
+    invoice1 = create(:invoice, merchant_id: merchant.id)
+    create(:invoice_item, invoice_id: invoice1.id, unit_price: 10000, quantity: 2)
+    create(:transaction, invoice_id: invoice1.id, result: 'success')
+    invoice2 = create(:invoice, merchant_id: merchant.id)
+    create(:invoice_item, invoice_id: invoice2.id, unit_price: 10000)
+    create(:transaction, invoice_id: invoice2.id, result: 'success')
+    invoice3 = create(:invoice, merchant_id: merchant.id)
+    create(:invoice_item, invoice_id: invoice3.id, unit_price: 10000)
+    create(:transaction, invoice_id: invoice3.id, result: 'failed')
+
+    expect(merchant.total_revenue).to eq('300.00')
+  end
 
 end
