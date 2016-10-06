922b92ec4266caf0fbd964c956a26015da04973c
Ryan Workman <rdavid1099@gmail.com>
2016-10-06 15:33:59 -0600

    Adds unit tests for items

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,12 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  it 'returns the best day' do
-    
+  it 'returns the best selling day for an item' do
+    item = create(:item)
+    invoice1 = create(:invoice, created_at: '2012-03-23T10:55:29.000Z')
+    invoice_item1 = create(:invoice_item, invoice_id: invoice1.id, item_id: item.id, quantity: 2)
+    create(:transaction, invoice_id: invoice1.id, result: 'success')
+    invoice2 = create(:invoice, created_at: '2012-04-23T10:55:29.000Z')
+    invoice_item2 = create(:invoice_item, invoice_id: invoice2.id, item_id: item.id)
+    create(:transaction, invoice_id: invoice2.id, result: 'success')
+
+    actual = item.best_day
+
+    expect(actual).to eq('2012-03-23T10:55:29.000Z')
+  end
+
+  it 'returns the most recent of best days' do
+    item = create(:item)
+    invoice1 = create(:invoice, created_at: '2012-03-23T10:55:29.000Z')
+    invoice_item1 = create(:invoice_item, invoice_id: invoice1.id, item_id: item.id, quantity: 2)
+    create(:transaction, invoice_id: invoice1.id, result: 'success')
+    invoice2 = create(:invoice, created_at: '2012-04-23T10:55:29.000Z')
+    invoice_item2 = create(:invoice_item, invoice_id: invoice2.id, item_id: item.id, quantity: 2)
+    create(:transaction, invoice_id: invoice2.id, result: 'success')
+
+    actual = item.best_day
+
+    expect(actual).to eq('2012-04-23T10:55:29.000Z')
   end
 end
-
-
-# GET /api/v1/items/:id/best_day
-# GET /api/v1/items/most_items?quantity=x
-# GET /api/v1/items/most_revenue?quantity=x
