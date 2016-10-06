a4c1dc64b38411d8030d69f950518c7f347d9a95
Ryan Workman <rdavid1099@gmail.com>
2016-10-06 15:07:58 -0600

    Adds functionality to return the best selling day for an item

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,16 @@ class Item < ApplicationRecord
   def unit_price_in_dollars
     '%.2f' % (self.unit_price / 100.0)
   end
+
+  def best_day
+    woo = self.invoices.joins('INNER JOIN transactions
+                               ON transactions.invoice_id = invoices.id')
+                        .where('transactions.result = ?', 'success')
+                        .select('invoices.created_at, SUM(invoice_items.quantity) AS items_sold')
+                        .group('invoices.id')
+                        .order('items_sold DESC, invoices.created_at DESC')
+                        .limit(1)
+                        .first
+                        .created_at
+  end
 end
